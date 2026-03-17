1e07eeafa07549c90ecb2957bdb6a4115bb2bdb1
ktyudha <ktyudha@gmail.com>
2026-03-17 19:01:27 +0700

    feat: exec dir

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -49,6 +49,7 @@ export class WhatsAppService {
         // executablePath: "/opt/homebrew/bin/chromium",
         // Linux
         // executablePath: "/usr/bin/chromium",
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
